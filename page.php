b501b3765bcfe13d0a4336eefce6fde52d0fe57b
Anti Roots <roots79@users.noreply.github.com>
2021-03-05 15:48:11 +0200

    Esimesed PHP proovimised

---
+++ page.php
@@ -0,0 +1,28 @@
+<?php
+$myname="Anti Roots";
+$currenttime=date("d.m.Y H:i:s");
+$timehtml="\n <p> Lehe avamise hetkel oli: ".$currenttime.".</p>\n";
+$semesterbegin= new DateTime("2021-1-25");
+$semesterend= new DateTime("2021-6-30");
+$semesterduration=$semesterbegin->diff($semesterend);
+$semesterdurationday=$semseterduration->format("%r%a");
+$semesterdurhtml="\n <p>2021 kevadsemestri kestus on " .$semsesterduration." päeva. </p> \n";
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+	<meta charset="utf-8">
+	<title>Veebirakendused ja nende loomine 2021</title>
+</head>
+<body>
+	<h1>
+	<?php
+	 echo $myname;
+	?></h1>
+	<p>See leht on valminud õppetöö raames!</p>
+<?php
+ echo $timehtml;
+ echo $semesterdurhtml;
+?>
+</body>
+</html>
