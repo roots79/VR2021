e52e3533c19f4b6a98c4ea0a30f0f792b4dd7937
Anti Roots <roots79@users.noreply.github.com>
2021-03-05 17:26:24 +0200

    Esimesed PHP proovimised teine tund

--- page.php
+++ page.php
@@ -1,12 +1,44 @@
 <?php
-$myname="Anti Roots";
-$currenttime=date("d.m.Y H:i:s");
-$timehtml="\n <p> Lehe avamise hetkel oli: ".$currenttime.".</p>\n";
-$semesterbegin= new DateTime("2021-1-25");
-$semesterend= new DateTime("2021-6-30");
-$semesterduration=$semesterbegin->diff($semesterend);
-$semesterdurationday=$semseterduration->format("%r%a");
-$semesterdurhtml="\n <p>2021 kevadsemestri kestus on " .$semsesterduration." päeva. </p> \n";
+	$myname = "Anti Roots";
+	$currenttime = date("d.m.Y H:i:s");
+	$timehtml = "\n <p>Lehe avamise hetkel oli: " .$currenttime .".</p> \n";
+	$semesterbegin = new DateTime("2021-1-25");
+	$semesterend = new DateTime("2021-6-30");
+	$semesterduration = $semesterbegin->diff($semesterend);
+	$semesterdurationdays = $semesterduration->format("%r%a");
+	$semesterdurhtml = "\n <p>2021 kevadsemestri kestus on " .$semesterdurationdays ." päeva.</p> \n";
+	$today = new DateTime("now");
+	$fromsemesterbegin = $semesterbegin->diff($today);
+	$fromsemesterbegindays = $fromsemesterbegin->format("%r%a");
+	
+	if($fromsemesterbegindays <= $semesterdurationdays){
+		$semesterprogress = "\n"  .'<p>Semester edeneb: <meter min="0" max="' .$semesterdurationdays .'" value="' .$fromsemesterbegindays .'"></meter>.</p>' ."\n";
+		//<p>Semester edeneb: <meter min="0" max="156" value="35"></meter>
+	} else {
+		$semesterprogress = "\n <p>Semester on lõppenud.</p> \n";
+	}
+	
+	//loeme piltide kataloogi sisu
+	$picsdir = "pics/";
+	$allfiles = array_slice(scandir($picsdir), 2);
+	//echo $allfiles[5];
+	//var_dump($allfiles);
+	$allowedphototypes=["image/jpeg", "image/png"];
+	$picfiles=[];
+
+	foreach($allfiles as $file){
+		$fileinfo = getimagesize($picsdir .$file);
+		//var_dump($fileinfo);
+		if(isset($fileinfo["mime"])){
+			if(in_array($fileinfo["mime"], $allowedphototypes)){
+				array_push($picfiles, $file);
+			}
+		}
+	}
+	
+	$photocount = count($picfiles);
+	$photonum = mt_rand(0, $photocount-1);
+	$randomphoto = $picfiles[$photonum];
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -17,12 +49,16 @@ $semesterdurhtml="\n <p>2021 kevadsemestri kestus on " .$semsesterduration." pä
 <body>
 	<h1>
 	<?php
-	 echo $myname;
-	?></h1>
+		echo $myname;
+	?>
+	</h1>
 	<p>See leht on valminud õppetöö raames!</p>
-<?php
- echo $timehtml;
- echo $semesterdurhtml;
-?>
+	<?php
+		echo $timehtml;
+		echo $semesterdurhtml;
+		echo $semesterprogress;
+	?>
+	<img src="<?php echo $picsdir .$randomphoto; ?>" alt="vaade Haapsalus">
+	
 </body>
 </html>
