b9509dc246710185b64a3f49f884e03a9db4109a
Anti Roots <roots79@users.noreply.github.com>
2021-04-15 15:16:01 +0300

    Kodune_ylesanne_2_uudiste_lugemine

--- readnews.php
+++ readnews.php
@@ -4,6 +4,8 @@ require_once "../../../conf.php";
 
 
 
+
+
 		function read_news()
 		{
 
@@ -12,15 +14,27 @@ require_once "../../../conf.php";
 			//m22rame suhtluseks kodeeringu
 			$conn->set_charset("utf-8");
 			// valmistan SQL käsu
-			$stmt = $conn->prepare("SELECT VR2021_news_title,VR2021_news_content,VR2021_news_author FROM VR2021_news");
+			if (isset($_POST["news_output_num"])) 
+				{
+					$news_limit = $_POST["news_output_num"];
+				}
+				else 
+				{
+					$news_limit = 3; // uudiste kuvamise vaike v22rtus
+				}
+			   
+			$stmt = $conn->prepare("SELECT VR2021_news_title,VR2021_news_content,VR2021_news_author,VR2021_added FROM VR2021_news ORDER BY VR2021_news_id DESC LIMIT ?");
+			$stmt -> bind_param("i",$news_limit); 
 			echo $conn->error;
 			//i-integer s-string d- decimal
-			$stmt->bind_result($news_title_from_db,$news_content_from_db,$news_author_from_db);
+			$stmt->bind_result($news_title_from_db,$news_content_from_db,$news_author_from_db,$added_from_db);
 			$stmt->execute();
 			$raw_news_html =null;
 			while ($stmt-> fetch())
 			{
 				$raw_news_html .= "\n <h2> " .$news_title_from_db. "</h2>";
+				$news_date = new DateTime($added_from_db);
+				$raw_news_html .= "\n <h5>Uudis lisatud: ".$news_date->format('d.m.Y H:i:s')."</h5>"; 
 				$raw_news_html .= "\n <p> " .nl2br($news_content_from_db)."</p>";
 				$raw_news_html .= "\n <p> Edastas:" ;
 				if(!empty($news_author_from_db))
@@ -29,7 +43,7 @@ require_once "../../../conf.php";
 				} 
 				else
 				{
-					$raw_news_html .= "Vari autor";
+					$raw_news_html .= "Variautor";
 				}
 				$raw_news_html .="</p>";
 			}
@@ -49,10 +63,15 @@ require_once "../../../conf.php";
 </head>
 <body>
 	<h1>
-	Uudisete lisamine
+	Uudisete lugemine
 	</h1>
 	<p>See leht on valminud õppetöö raames!</p>
 	<hr>
+	<p>Mitut uudiste lehel kuvada:</p>
+    <form  action="" method="post" >
+    <INPUT type="number" min="1" max="10" value="<?php echo $news_limit; ?>" name="news_output_num" >
+	<input type="submit" value="Vali"> 
+    </form>
 	<?php echo $news_html; ?>
 </body>
 </html>
